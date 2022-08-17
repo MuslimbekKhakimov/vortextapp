7e7907ebd351ca8d105a64b1ff05227ef117d00b
Abdukarim2 <abdukarimhakimov461@gmail.com>
2022-08-17 22:08:53 +0500

    end effective

--- src/components/ui/global/Effective.vue
+++ src/components/ui/global/Effective.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="effective" @click="effectPulse">
+    <div class="effective" ref="effective" @click="effectRipple($event)">
         <slot></slot>
-        <div class="effective-pulse" ref="effect_pulse"></div>
     </div>
 </template>
 <script>
 export default {
     name:"Effective",
     methods: {
-        effectPulse(){
-            let element = this.$refs.effect_pulse;
-            let layery = event.layerY;
-            let layerx = event.layerX;
-            element.style = `transform:scale(2);top:${layery-10}px; left:${layerx-10}px;`
-            setTimeout(() => {
-                element.style = "opacity:0; transform:scale(0);"    
-            }, 600);
+        effectRipple(event){
+            let effective = this.$refs.effective;
+            let ripple = document.createElement('div');
+            if(effective.querySelectorAll('.ripple').length > 0){
+                effective.removeChild(effective.querySelector(".ripple"));
+            }
+            effective.appendChild(ripple);     
+            var d = Math.max(effective.clientWidth, effective.clientHeight);
+            ripple.style.width = ripple.style.height = d + 'px';
+            ripple.style.left = event.clientX - effective.offsetLeft - d / 2 + 'px';
+            ripple.style.top = event.clientY - effective.offsetTop - d / 2 + 'px'; 
+            ripple.classList.add('ripple');    
         }
-    }
+    }   
 }
 </script>
 <style>
@@ -26,19 +29,26 @@ export default {
         transition-property: background-color;
         transition-duration: 0.2s;
         transition-timing-function: ease;
+        cursor: pointer;
         overflow: hidden;
     }
-
     .effective:hover{
         background-color: var(--effective);
     }
-    .effective-pulse{
-        transition: all 0.5s;
+    .ripple{
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.1);
         position: absolute;
-        width: 100%;
-        height: 100%;
-        background-color: rgb(0, 0, 0, 0.07);
         transform: scale(0);
-        border-radius: 100px;
+        opacity: 1;
+        animation-name: ripple;
+        animation-duration: .6s;
+        animation-timing-function: ease-in;
+    }
+    @keyframes ripple{
+        to{
+            transform: scale(2.5);
+            opacity: 0;
+        }
     }
 </style>
